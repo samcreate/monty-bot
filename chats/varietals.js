d4615d940c8325a39c13b587259bcbc2318c1216
Aaron  McGuire <aaronmcguire@Aarons-MacBook-Pro.local>
2016-11-27 13:26:28 -0800

    logging

--- chats/varietals.js
+++ chats/varietals.js
@@ -38,6 +38,7 @@ module.exports = (bot) => {
     let varietal_id = data.id;
     console.log('!@---------------------get-winesbyvarietal', varietal_id)
     Sessions.instance.findOrCreate(payload.sender.id).then(({user, session}) => {
+      console.log('!@---------------------get-winesbyvarietal 1')
       db.Wines.findAll({
         where: {
           varietal_id: varietal_id,
@@ -48,9 +49,10 @@ module.exports = (bot) => {
         ]
       })
         .then((wines) => {
-
+          console.log('!@---------------------get-winesbyvarietal 2')
           let elements = [];
-          wines.forEach((wine) => {
+          wines.forEach((wine, index) => {
+            console.log('!@---------------------get-winesbyvarietal 3 ', index)
             let hero = 'https://s3-us-west-1.amazonaws.com/monty-prod/'+wine.hero || 'https://s3-us-west-1.amazonaws.com/monty-prod/92179bf9-e253-4ff2-b1a7-3fa56b5d969f.jpg'
             wine = wineListGen({
               id: wine.id,
@@ -61,9 +63,11 @@ module.exports = (bot) => {
               price: wine.price,
               vintage: wine.vintage
             });
+
             elements.push(wine);
           });
           // console.log(elements)
+          console.log('!@---------------------get-winesbyvarietal 4')
           chat.sendListTemplate(elements, {
             typing: true
           }).catch((err) => {
